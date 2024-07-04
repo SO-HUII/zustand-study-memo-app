44168c7ed88ada6c371e128bcf7226c06a6d1dba
sohui <sohi9203@gmail.com>
2024-07-04 17:44:43 +0900

    Feat: board 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import './App.css';
+import MemoBoard from "./components/MemoBoard";
+import MemoElem from "./components/MemoElem";
+import MemoInput from "./components/MemoInput";
+import { useMemoStore } from "./store/MemoList";
 
 function App() {
+  const { memoList } = useMemoStore();
   return (
-    <div>
-      
-    </div>
+    <>
+      <MemoBoard>
+        {memoList.length ? (
+          memoList.map((e) => {
+            return (
+              <MemoElem key={e.id} id={e.id}>
+                {e.content}
+              </MemoElem>
+            );
+          })
+        ) : (
+          <p>메모를 입력해주세요</p>
+        )}
+      </MemoBoard>
+      <MemoInput />
+    </>
   );
 }
 
